a812ec3d4b63d4a12fe93931f5e33d07f869f2d6
Nolan <nolanehs@gmail.com>
2018-01-16 08:55:50 -0500

    vars and print

---
+++ pythonpractice/exercise5_vars_and_print.py
@@ -0,0 +1,17 @@
+my_name = 'Nolan Byrnes'
+my_age = 28
+my_height = 75
+my_weight = 170
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+
+print(f"If I add {my_age}, {my_height}, and {my_weight}, I get {total}.")
